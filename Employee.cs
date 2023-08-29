45569c91079ed1b2578206775cb4883e697359be
praveen <praveenryali058@gmail.com>
2023-08-29 19:15:07 +0530

    first commit

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppPartialViewEx
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+
+        public string Designation { get; set; }
+        [Required]
+
+        public double Salary { get; set; }
+
+        public DateTime DOJ { get; set; }
+
+    }
+}
